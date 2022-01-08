9c6b69732d2a3b5ad0a9150cf7cba59fac631748
VanRar <vitalikf7@gmail.com>
2022-01-08 19:51:22 +0300

    add DAO and MyController

---
+++ src/main/java/org/vanrar/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,9 @@
+package org.vanrar.spring.mvc_hibernate_aop.dao;
+
+import org.vanrar.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+}
